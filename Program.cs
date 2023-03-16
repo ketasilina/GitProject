2e7bcc569e69d456db8f5fa938e189ba87b502de
eKeta Silina <keta.silina@gmail.com>
2023-03-16 20:58:37 +0200

    kk

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("He, World!");
 Console.WriteLine("He, World!");
+Console.WriteLine("He, World!");
+Console.WriteLine("Herold!");
