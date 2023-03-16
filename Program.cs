c382f020fa6ab78cd5024d8aa1d55fe60acdd78e
eKeta Silina <keta.silina@gmail.com>
2023-03-16 21:01:15 +0200

    aaa

--- Program.cs
+++ Program.cs
@@ -3,3 +3,6 @@ Console.WriteLine("He, World!");
 Console.WriteLine("He, World!");
 Console.WriteLine("He, World!");
 Console.WriteLine("Herold!");
+
+Console.WriteLine("He, World!");
+Console.WriteLine("Herold!");
